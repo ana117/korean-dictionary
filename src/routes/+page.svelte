189aa4e654fa2b1d1fdde081078c357c0bfc1f8a
ana117 <63540951+ana117@users.noreply.github.com>
2024-11-27 17:43:32 +0700

    feat: create search api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,10 @@
 	import VirtualKeyboard from '$lib/components/keyboard/VirtualKeyboard.svelte';
 	import type { KoreanWord } from '$lib/types';
 
+	let words = $state<KoreanWord[]>([]);
+	let isLoading = $state(false);
+	let error = $state('');
+
 	let showKeyboard = $state(false);
 	let searchQuery = $state('');
 	setContext('searchQuery', {
@@ -19,119 +23,39 @@
 	});
 
 	$effect(() => {
-		searchQuery = hangul.assemble(searchQuery.split(''));
+		const disassembled = hangul.disassemble(searchQuery);
+		searchQuery = hangul.assemble(disassembled);
 	});
 
 	const handleSearch = () => {
-		if (searchQuery) console.log('Search query:', searchQuery);
-	};
+		if (!searchQuery) return;
 
-	const mockResult: KoreanWord[] = [
-		{
-			code: '1',
-			koreanWord: '안녕',
-			translations: [
-				{
-					text: 'hello',
-					definition: 'a greeting'
-				},
-				{
-					text: 'hi',
-					definition: 'a greeting'
+		isLoading = true;
+		error = '';
+		words = [];
+
+		fetch('/search', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ word: searchQuery })
+		})
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error('Internal server error');
 				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑 사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑사랑 사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑 사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑사랑 사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑 사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		},
-		{
-			code: '2',
-			koreanWord: '사랑사랑 사랑',
-			translations: [
-				{
-					text: 'love',
-					definition: 'a feeling'
-				}
-			]
-		}
-	];
+				return res.json();
+			})
+			.then((data) => {
+				words = data;
+				isLoading = false;
+			})
+			.catch((err) => {
+				error = err.message;
+				isLoading = false;
+			});
+	};
 </script>
 
 <main
@@ -140,13 +64,40 @@
 	<SearchBar handleShowKeyboard={() => (showKeyboard = !showKeyboard)} {handleSearch} />
 
 	<div class="mt-8 grow overflow-y-auto px-6">
-		<div
-			class="grid grid-cols-1 content-start gap-x-4 gap-y-16 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-8"
-		>
-			{#each mockResult as word, i}
-				<ResultCard {word} isFirst={i === 0} />
-			{/each}
-		</div>
+		{#if isLoading}
+			<div class="flex h-64 items-center justify-center">
+				<div class="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-primary"></div>
+			</div>
+		{:else if error}
+			<div
+				class="mt-8 flex flex-col items-center justify-center gap-4 text-center text-2xl font-semibold"
+			>
+				<p class="text-red-600">Internal Server Error</p>
+				<p>
+					Please try again later or contact me through
+					<a
+						href="https://github.com/ana117/korean-dictionary/issues"
+						target="_blank"
+						class="underline-primary text-primary underline-offset-2 hover:underline">GitHub</a
+					>
+				</p>
+			</div>
+		{:else if words.length}
+			<div
+				class="grid grid-cols-1 content-start gap-x-4 gap-y-16 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-8"
+			>
+				{#each words as word, i}
+					<ResultCard {word} isFirst={i === 0} />
+				{/each}
+			</div>
+		{:else}
+			<div
+				class="flex flex-col items-center justify-center gap-4 text-center text-2xl font-semibold"
+			>
+				<p class="font-semibold">No results found</p>
+				<p class="text-text/50">Please try another search term</p>
+			</div>
+		{/if}
 	</div>
 </main>
 
