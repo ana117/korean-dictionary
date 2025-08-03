e86757b7350ffdbc2e82a83ced055d95f9ad38a1
ana117 <63540951+ana117@users.noreply.github.com>
2025-08-03 21:27:38 +0700

    feat: improve styling and add placeholder if no equivalent expression exist (empty string)

--- src/lib/components/ResultCard.svelte
+++ src/lib/components/ResultCard.svelte
@@ -17,8 +17,15 @@
 	<p class="fonr-bold text-3xl text-text">
 		{word.koreanWord}
 	</p>
-	<ul>
+	<ul class="hidden md:list-item">
 		{#each word.translations as translation}
+			<li>
+				{translation.text}
+			</li>
+		{/each}
+	</ul>
+	<ul class="md:hidden">
+		{#each word.translations.slice(0, 3) as translation}
 			<li>
 				{translation.text}
 			</li>
@@ -26,16 +33,16 @@
 	</ul>
 </button>
 
-<dialog bind:this={dialog} class="w-5/6 rounded-md bg-background px-8 py-5 md:w-1/2">
+<dialog bind:this={dialog} class="w-5/6 rounded-md bg-background px-8 md:w-1/2">
 	<div class="flex h-full flex-col items-center">
 		<div
-			class="sticky top-0 flex h-16 w-full justify-center border-b-2 border-foreground/25 bg-background"
+			class="sticky top-0 flex h-16 w-full justify-center border-b-2 border-foreground/25 bg-background pt-5"
 		>
 			<h2 class="text-3xl font-bold">
 				{word.koreanWord}
 			</h2>
 			<a href={externalLink} target="_blank" rel="noopener noreferrer" class="group relative">
-				<span class="sr-only"> Extrenal link to the Korean Dictionary </span>
+				<span class="sr-only"> External link to the Korean Dictionary </span>
 				<svg
 					viewBox="0 0 24 24"
 					class="absolute -right-6 top-0 h-5 w-5 rounded-sm p-0.5 group-hover:scale-110 group-hover:bg-primary group-hover:fill-background"
@@ -52,7 +59,9 @@
 			<ul>
 				{#each word.translations as translation, i}
 					<li class="mb-4 text-xl">
-						<span class="font-bold">{i + 1}. {translation.text}</span>
+						<span class="font-bold">
+							{i + 1}. {translation.text === '' ? '(no equivalent expression)' : translation.text}
+						</span>
 						<br />
 						<span>{translation.definition}</span>
 					</li>
@@ -61,7 +70,7 @@
 		</div>
 
 		<div
-			class="sticky bottom-0 flex h-16 w-full justify-center border-t-2 border-foreground/25 bg-background"
+			class="sticky bottom-0 flex h-16 w-full justify-center border-t-2 border-foreground/25 bg-background pb-5"
 		>
 			<button
 				on:click={() => dialog.close()}
